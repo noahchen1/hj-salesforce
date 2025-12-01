5943bd1cfe6a0283dd46c0eafc6e283df1088d66
Noah Chen <nchen@hamco.local>
2025-12-01 11:07:08 -0500

    minor style update to open repair orders and first commit for open special orders.

---
+++ force-app/main/default/lwc/openSpecialOrders/openSpecialOrders.js
@@ -0,0 +1,36 @@
+import { LightningElement, wire, track } from "lwc";
+import getOpenSpecialOrders from "@salesforce/apex/openSpecialOrdersController.getOpenSpecialOrders";
+
+export default class OpenSpecialOrders extends LightningElement {
+  @wire(getOpenSpecialOrders)
+  wiredData;
+
+  get rows() {
+    const data = this.wiredData?.data || [];
+
+    return data.map((r) => ({
+      id: r.Id,
+      date: this.formateDate(r.breadwinner_ns__CreatedDate__c),
+      customer: r?.breadwinner_ns__Entity__r?.Name || "",
+      specialDate: this.formateDate(r.ncf_body_special_date__c),
+      salesRep: r.breadwinner_ns__SalesRepName__c,
+      status: r.ncf_body_special_statuses__c,
+      notes: r.ncf_body_special_comments__c,
+      document: r.Name
+    }));
+  }
+
+  get columns() {
+    return [
+      { label: "Customer", fieldName: "customer" },
+      { label: "Date", fieldName: "date" },
+      { label: "Document", fieldName: "document" },
+      { label: "Need By Date", fieldName: "specialDate" },
+      { label: "Sales Rep", fieldName: "salesRep" },
+      { label: "status", fieldName: "status" },
+      { label: "notes", fieldName: "notes" }
+    ];
+  }
+
+  formateDate = (date) => (date ? new Date(date).toLocaleDateString() : "");
+}
