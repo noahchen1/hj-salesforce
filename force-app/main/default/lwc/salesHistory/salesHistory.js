52465d025fbf3a59c0bdf38ace5b21c37dffcb08
Noah Chen <nchen@hamco.local>
2026-02-24 12:51:57 -0500

    added changes for sales history widget.

--- force-app/main/default/lwc/salesHistory/salesHistory.js
+++ force-app/main/default/lwc/salesHistory/salesHistory.js
@@ -1,78 +1,108 @@
 import { LightningElement, track, wire } from "lwc";
 import getSalesHistory from "@salesforce/apex/SalesHistory.getSalesHistory";
-import LightningAlert from "lightning/alert";
 
 export default class SalesHistory extends LightningElement {
-  constructor() {
-    super();
-    this.nextCursor = null;
-    this.prevCursor = null;
-    this.hasNext = false;
-    this.hasPrev = false;
-  }
-
-  @track isLoading = false;
-  @track sortBy = "trandate";
-  @track sortDirection = "desc";
-  @track rows = [];
+  @track isLoading = true;
   @track customer = null;
+  @track pageNumber = 1;
   @track pageSize = 25;
-  @track direction = "NEXT";
-  @track cursorJson = null;
+  @track sortBy = "totalSpend";
+  @track sortDirection = "desc";
+  @track tableDataLoaded = false;
+
+  tableData = [];
+  wireError;
 
   @wire(getSalesHistory, {
     customer: null,
     pageSize: "$pageSize",
-    direction: "$direction",
-    cursorJson: "$cursorJson",
+    offsetSize: "$offset",
     sortBy: "$sortBy",
     sortDirection: "$sortDirection"
   })
-  wiredData({ data, error }) {
-    if (error) {
-      LightningAlert.open({
-        message:
-          "There's a problem when loading the page, please contact administrators!",
-        theme: "error",
-        label: "Error!"
-      });
-
-      console.error(data.error);
-
-      return;
-    }
-
+  wiredSalesHistory({ data, error }) {
     if (data) {
-      this.rows = data.rows || [];
-      this.nextCursor = data.nextCursor;
-      this.prevCursor = data.prevCursor;
-      this.hasNext = !!data.hasNext;
-      this.hasPrev = !!data.hasPrev;
+      this.tableData = data;
+      this.wireError = undefined;
+      this.isLoading = false;
+    } else if (error) {
+      this.tableData = [];
+      this.isLoading = false;
+      this.wireError = error;
     }
+  }
+
+  get offset() {
+    return (this.pageNumber - 1) * this.pageSize;
+  }
+
+  get rows() {
+    const data = this.tableData || [];
+
+    const mappedData = data.map((r) => {
+      return {
+        customer: r.customer,
+        firstName: r.first_name,
+        lastName: r.last_name,
+        email: r.email,
+        phone: r.phone,
+        mobile: r.mobile,
+        street: r.street,
+        city: r.city,
+        state: r.state,
+        zip: r.zip,
+        country: r.country,
+        subsidiary: r.subsidiary,
+        salesRep: r.sales_rep,
+        birthday: r.birthday,
+        anniversary: r.anniversary,
+        ranking: r.ranking,
+        isNewCustomer: r.is_new_customer,
+        accountSource: r.account_source,
+        lifetimeSpend: r.lifetime_spend,
+        totalSpend: r.total_spend
+      };
+    });
+
+    return mappedData;
   }
 
   get columns() {
     return [
-      { label: "Id", fieldName: "id" },
-      { label: "Item", fieldName: "item" },
-      { label: "Quantity", fieldName: "quantity", type: "number" },
+      { label: "Customer", fieldName: "customer" },
+      { label: "First Name", fieldName: "firstName" },
+      { label: "Last Name", fieldName: "lastName" },
+      { label: "Email", fieldName: "email" },
+      { label: "Phone", fieldName: "phone" },
+      { label: "Mobile", fieldName: "mobile" },
+      { label: "Street", fieldName: "street" },
+      { label: "Zip", fieldName: "zip" },
+      { label: "Country", fieldName: "country" },
+      { label: "Subsidiary", fieldName: "subsidiary" },
+      { label: "Sales Rep", fieldName: "salesRep" },
       {
-        label: "Amount",
-        fieldName: "amount",
-        type: "currency",
-        sortable: true
-      },
-      { label: "Vendor", fieldName: "preferredVendor" },
-      { label: "Location", fieldName: "location" },
-      { label: "Department", fieldName: "department" },
-      { label: "Division", fieldName: "division" },
-      {
-        label: "Date",
-        fieldName: "trandate",
+        label: "Birthday",
+        fieldName: "birthday",
         type: "date-local",
         sortable: true
-      }
+      },
+      {
+        label: "Anniversary",
+        fieldName: "anniversary",
+        type: "date-local"
+      },
+      { label: "Ranking", fieldName: "ranking" },
+      { label: "New Customer", fieldName: "isNewCustomer" },
+      { label: "Lead Source", fieldName: "accountSource" },
+      { label: "Lifetime Spend", fieldName: "lifetimeSpend" },
+      { label: "Total Spend", fieldName: "totalSpend", sortable: true }
     ];
+  }
+
+  checkLoadingState() {
+    if (this.tableDataLoaded) {
+      this.isLoading = false;
+    }
   }
 
   handleNext() {
