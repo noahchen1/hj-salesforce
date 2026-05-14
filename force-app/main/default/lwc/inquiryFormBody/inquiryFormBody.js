ec9423a3860825c1838ca6b1556b1f9b793a10f0
nchen <nuoya1996@gmail.com>
2026-05-14 14:43:26 -0400

    added inquiry form.

--- force-app/main/default/lwc/inquiryFormBody/inquiryFormBody.js
+++ force-app/main/default/lwc/inquiryFormBody/inquiryFormBody.js
@@ -1,3 +1,52 @@
-import { LightningElement } from "lwc";
+import { LightningElement, wire } from "lwc";
+import getLocations from "@salesforce/apex/DropdownDataController.getLocations";
+import { processPicklistData } from "c/salesOrderUtils";
 
-export default class InquiryFormBody extends LightningElement {}
+export default class InquiryFormBody extends LightningElement {
+  location = "";
+  locationOptions = [];
+
+  isLoaded = false;
+  isLocationLoaded = false;
+
+  @wire(getLocations)
+  handleLocations({ data, error }) {
+    if (error) {
+      console.error("Error fetching location options: ", error);
+
+      this.locationOptions = [{ label: "Select", value: "" }];
+      this.isLocationLoaded = true;
+      this.checkLoadingState();
+      return;
+    }
+
+    const { options } = processPicklistData(data);
+    this.locationOptions = options;
+    this.isLocationLoaded = true;
+    this.checkLoadingState();
+  }
+
+  async handleLookupSearch() {}
+
+  handleLookupSelect() {}
+
+  handleComboboxChange(e) {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    this[name] = value;
+  }
+
+  handleInputChange(e) {
+    const type = e.target.dataset.type;
+    const value = e.target.value;
+
+    this[type] = value;
+  }
+
+  checkLoadingState() {
+    if (this.isLocationLoaded) {
+      this.isLoading = false;
+    }
+  }
+}
--- force-app/main/default/lwc/inquiryFormItems/inquiryFormItems.js
+++ force-app/main/default/lwc/inquiryFormItems/inquiryFormItems.js
@@ -1,3 +1,44 @@
-import { LightningElement } from "lwc";
+import { LightningElement, api } from "lwc";
 
-export default class InquiryFormItems extends LightningElement {}
+export default class InquiryFormItems extends LightningElement {
+  @api sectionTitle = "Watch Fields";
+  @api modelLabel;
+  @api isModelRequired;
+  @api nameLabel;
+  @api isNameRequired;
+  @api linkLabel;
+  @api isLinkRequired;
+
+  model = "";
+  name = "";
+  link = "";
+  isPriority = false;
+  isOpenDial = false;
+  showFieldsToggle = false;
+
+  @api
+  get toggleFieldsVisible() {
+    return this.showFieldsToggle;
+  }
+
+  set toggleFieldsVisible(value) {
+    this.showFieldsToggle = value;
+  }
+
+  // renderedCallback() {
+  //   console.log(JSON.stringify(this.showFieldsToggle));
+  //   console.log("getter below:");
+  //   console.log(JSON.stringify(this.areFieldsVisisble));
+  // }
+
+  handleInputChange(e) {
+    const type = e.target.dataset.type;
+    const value = e.target.value;
+
+    this[type] = value;
+  }
+
+  handleHeaderClick() {
+    this.showFieldsToggle = !this.showFieldsToggle;
+  }
+}
